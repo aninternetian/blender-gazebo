c785cc4f57a9d9b9309fe946bae3dfbb653a3a9d
iamironcat <roselle.carmen@gmail.com>
2019-04-22 22:35:06 +0800

    added updated script for automation

--- blend2gz.py
+++ blend2gz.py
@@ -1,9 +1,17 @@
 # blender to gazebo cordinates converter
 # paste blender coordinates here
 
-blend_x = float(input("Blender X = "))
-blend_y = float(input("Blender Y = "))
-blend_z = float(input("Blender Z = "))
+import bpy
+
+selected = bpy.data.objects[0]
+
+blend_x = selected.location[0]
+blend_y = selected.location[1]
+blend_z = selected.location[2]
+
+# blend_x = float(input("Blender X = "))
+# blend_y = float(input("Blender Y = "))
+# blend_z = float(input("Blender Z = "))
 
 gz_x = (blend_y * -1) /100
 gz_y = (blend_x * -1) /100
