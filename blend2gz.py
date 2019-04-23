2380eee0bbc9e185b0e95caa9216250b3731214a
iamironcat <roselle.carmen@gmail.com>
2019-04-23 21:55:12 +0800

    added decor

--- blend2gz.py
+++ blend2gz.py
@@ -3,8 +3,10 @@
 import bpy
 
 bpy.ops.script.python_file_run(filepath="/snap/blender/20/2.79/scripts/presets/units_length/meters.py")
+# will be working for all systems eventually
 
 models = bpy.data.objects
+print("~~~~~~~~~~")
 
 for model in models:
     print(model)
@@ -16,11 +18,14 @@ for model in models:
     blend_z = model.location[2]
     print("Original_Z: " + str(blend_z))
 
-    gz_x = (blend_y * -1)
-    gz_y = (blend_x * -1)
+    gz_x = blend_y * -1
+    gz_y = blend_x * -1
     gz_z = blend_z
 
     conv = [gz_x, gz_y, gz_z]
     
     for c in conv:
         print(round(c, 3))
+        # to print in a better format
+
+print("~~~~~~~~~~")
