2321bed5fae55abeb0905f46de433b58e6bb5af9
iamironcat <roselle.carmen1985@gmail.com>
2019-04-24 12:44:37 +0800

    updated script to look pretty

--- blend2gz.py
+++ blend2gz.py
@@ -1,9 +1,10 @@
-'''blender to gazebo cordinates converter'''
+'''Writes all your blender models into the gazebo SDF'''
 
 import bpy
+import os 
 
-bpy.ops.script.python_file_run(filepath="/snap/blender/20/2.79/scripts/presets/units_length/meters.py")
-# will be working for all systems eventually
+dir_path = os.path.dirname(os.path.realpath("/meters.py"))
+bpy.ops.script.python_file_run(filepath=dir_path)
 
 models = bpy.data.objects
 print("~~~~~~~~~~")
@@ -11,21 +12,19 @@ print("~~~~~~~~~~")
 for model in models:
     print(model)
 
-    blend_x = model.location[0]
-    print("Original_X: " + str(blend_x))
-    blend_y = model.location[1]
-    print("Original_Y: " + str(blend_y))
-    blend_z = model.location[2]
-    print("Original_Z: " + str(blend_z))
-
-    gz_x = blend_y * -1
-    gz_y = blend_x * -1
-    gz_z = blend_z
+    gz_x = (model.location[1] * -1) + 0
+    gz_y = (model.location[0] * -1) + 0
+    gz_z = (model.location[2]) + 0
 
     conv = [gz_x, gz_y, gz_z]
     
     for c in conv:
         print(round(c, 3))
-        # to print in a better format
 
 print("~~~~~~~~~~")
+
+# TO-DOS
+###
+# button in blender
+# format results in one line
+# print only object name
