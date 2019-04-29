15fc7fe05a3524b5b39a46d87735b4e72ab4a269
iamironcat <roselle.carmen1985@gmail.com>
2019-04-29 15:45:42 +0800

    updated

--- blend2gz.py
+++ blend2gz.py
@@ -3,30 +3,39 @@
 import bpy
 import argparse
 
-def transform(num):
+def set_unit():
+    # this section changes unit
     scene = bpy.context.scene
 
     scene.unit_settings.system = 'METRIC'
     scene.unit_settings.scale_length = 1.0
 
+    check_models()
+
+def check_models():
+
     models = bpy.data.objects
 
     f = open(args.output, 'w')
 
-    return "%.2f" % num if num else '0'
-
     for model in models:
-        f.write(model.name)
-
-        coords = model.location
-        position = []
-
-        position.append(coords[1] * -1 + 0)
-        position.append(coords[0] * -1 + 0)
-        position.append(coords[2] + 0)
-        f.write(' '.join(map(transform, position)))
+        print_coords(model, f)
 
     f.close()
+
+def transform(num):
+    return "%.2f" % num if num else '0'
+
+def print_coords(model, f):
+    f.write(model.name + "\n")
+
+    coords = model.location
+    position = []
+
+    position.append(coords[0] * -1 + 0)
+    position.append(coords[1] * -1 + 0)
+    position.append(coords[2] + 0)
+    f.write(' '.join(map(transform, position)) + "\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -41,6 +50,4 @@ if __name__ == "__main__":
 
     bpy.ops.wm.open_mainfile(filepath=args.input)
 
-####################
-# argparse
-# sdformatpy
+    set_unit()
--- blend2gz_2.py
+++ blend2gz_2.py
@@ -1,27 +1,56 @@
 '''Translates all objects location in your scene into the gazebo SDF'''
 
 import bpy
-
-scene = bpy.context.scene
-
-scene.unit_settings.system = 'METRIC'
-scene.unit_settings.scale_length = 1.0
-
-models = bpy.data.objects
+import argparse
 
 def transform(num):
     return "%.2f" % num if num else '0'
 
-for model in models:
-    print(model.name)
-    
+def print_coords(model, f):
+    f.write("\n" + model.name + "\n")
+
     coords = model.location
     position = []
 
     position.append(coords[1] * -1 + 0)
     position.append(coords[0] * -1 + 0)
     position.append(coords[2] + 0)
-    print(' '.join(map(transform, position)))
+    f.write(' '.join(map(transform, position)))
+
+def check_models():
+
+    models = bpy.data.objects
+
+    f = open(args.output, 'w')
+
+    for model in models:
+        print_coords(model, f)
+
+    f.close()
+
+def unit_meter():
+    # this section changes unit
+    scene = bpy.context.scene
+
+    scene.unit_settings.system = 'METRIC'
+    scene.unit_settings.scale_length = 1.0
+
+    check_models()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    _, all_arguments = parser.parse_known_args()
+    double_dash_index = all_arguments.index('--')
+    script_args = all_arguments[double_dash_index + 1:]
+
+    parser.add_argument('input', help='Input .blend file')
+    parser.add_argument('output', help='Output .txt file for the coordinates')
+    args, _ = parser.parse_known_args(script_args)
+
+    bpy.ops.wm.open_mainfile(filepath=args.input)
+
+    unit_meter()
 
 ##########
 # TO-DOS #
