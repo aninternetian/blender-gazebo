92dd5ef205daabf3a0c5ca48f49641b72dd261c6
iamironcat <roselle.carmen1985@gmail.com>
2019-05-10 14:26:39 +0800

    testing with pretty code

--- examal_test.py
+++ examal_test.py
@@ -1,4 +1,5 @@
 import xml.etree.cElementTree as ET
+from ElementTree_pretty import prettify     # used for testing
 
 # write xml file - https://stackoverflow.com/questions/3605680/creating-a-simple-xml-file-using-python
 
@@ -8,4 +9,4 @@ model = ET.SubElement(sdf, 'model')
 model.set('name', 'default')    # model.append(robot_name)
 
 tree = ET.ElementTree(sdf)
-tree.write('model.sdf')
+prettify(tree).write('model.sdf')
