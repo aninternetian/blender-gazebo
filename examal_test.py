789bde1adc48ac69cf54964a8b37dc4c5d198467
iamironcat <roselle.carmen@gmail.com>
2019-05-12 19:29:19 +0800

    testing git commit on vs coe

--- examal_test.py
+++ examal_test.py
@@ -1,12 +1,28 @@
-import xml.etree.cElementTree as ET
-from ElementTree_pretty import prettify     # used for testing
+import xml.etree.ElementTree as ET
 
 # write xml file - https://stackoverflow.com/questions/3605680/creating-a-simple-xml-file-using-python
+
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
 
 sdf = ET.Element('sdf', {'version': '1.6'})
 model = ET.SubElement(sdf, 'model')
 
-model.set('name', 'default')    # model.append(robot_name)
+model.set('name', 'robot_name')    # model.append()
 
 tree = ET.ElementTree(sdf)
-prettify(tree).write('model.sdf')
+indent(sdf)
+tree.write('model.sdf', xml_declaration=True, encoding='utf-8')
