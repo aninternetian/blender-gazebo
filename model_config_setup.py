c99b0994963f928eff6b525a716eccf87d98e6c0
iamironcat <roselle.carmen1985@gmail.com>
2019-06-26 11:07:42 +0800

    setting up model.config

--- model_config_setup.py
+++ model_config_setup.py
@@ -22,6 +22,26 @@ name = ET.SubElement(model, 'name')
 # name.text = model_name())     # retrieve from common_functions.py
 name.text = 'file_name'
 
+version = ET.SubElement(model, 'version')
+version.text = '1.0'
+
+sdf = ET.SubElement(model, 'sdf')
+sdf.set('version' , '1.6')
+sdf.text = 'model.sdf'
+
+# section for author
+
+author = ET.SubElement(model, 'author')
+
+name = ET.SubElement(author, 'name')
+name.text = 'Name'  # retrieve name from local file (user need to create file for name)
+
+email = ET.SubElement(author, 'email')
+email.text = 'email@email.com'  # retrieve email from local file as well
+
+description = ET.SubElement(model, 'description')
+description.text = 'description about the model'    # retrieve from field in blender addon
+
 tree = ET.ElementTree(model)
 indent(model)
 tree.write('model.config', xml_declaration=True, encoding='utf-8')
